809c0cd02c538497dd1ca14a32937265de7732cb
Håvard Pedersen Brandal <haavard.brandal@gmail.com>
2025-10-29 19:41:20 +0100

    Several changes: - Handle maps that have non-zero origins - Add argparse to allow users specify several arguments like number of iterations, planners, and start/goal - Incase start and goal is specified these are used instead of the randomized start/goal functions

--- tools/planner_benchmarking/metrics.py
+++ tools/planner_benchmarking/metrics.py
@@ -16,7 +16,10 @@
 from geometry_msgs.msg import PoseStamped
 from nav2_simple_commander.robot_navigator import BasicNavigator
 import rclpy
+import sys
 from ament_index_python.packages import get_package_share_directory
+import argparse
+from functools import partial
 
 import math
 import os
@@ -43,7 +46,7 @@ def getPlannerResults(navigator, initial_pose, goal_pose, planners):
     return results
 
 
-def getRandomStart(costmap, max_cost, side_buffer, time_stamp, res):
+def getRandomStart(costmap, max_cost, side_buffer, time_stamp, res, origin):
     start = PoseStamped()
     start.header.frame_id = 'map'
     start.header.stamp = time_stamp
@@ -52,8 +55,9 @@ def getRandomStart(costmap, max_cost, side_buffer, time_stamp, res):
         col = randint(side_buffer, costmap.shape[1]-side_buffer)
 
         if costmap[row, col] < max_cost:
-            start.pose.position.x = col*res
-            start.pose.position.y = row*res
+            # Transform the starting vector given relative to the grid frame to the map frame
+            start.pose.position.x = col*res + origin[0]
+            start.pose.position.y = row*res + origin[1]
 
             yaw = uniform(0, 1) * 2*math.pi
             quad = euler2quat(0.0, 0.0, yaw)
@@ -65,7 +69,7 @@ def getRandomStart(costmap, max_cost, side_buffer, time_stamp, res):
     return start
 
 
-def getRandomGoal(costmap, start, max_cost, side_buffer, time_stamp, res):
+def getRandomGoal(costmap, max_cost, side_buffer, time_stamp, res, origin, start):
     goal = PoseStamped()
     goal.header.frame_id = 'map'
     goal.header.stamp = time_stamp
@@ -75,13 +79,13 @@ def getRandomGoal(costmap, start, max_cost, side_buffer, time_stamp, res):
 
         start_x = start.pose.position.x
         start_y = start.pose.position.y
-        goal_x = col*res
-        goal_y = row*res
+        goal_x = col*res + origin[0]
+        goal_y = row*res + origin[1]
         x_diff = goal_x - start_x
         y_diff = goal_y - start_y
         dist = math.sqrt(x_diff ** 2 + y_diff ** 2)
 
-        if costmap[row, col] < max_cost and dist > 3.0:
+        if costmap[row, col] < max_cost and dist > 1.0: # Make it a variable based on the size of the map
             goal.pose.position.x = goal_x
             goal.pose.position.y = goal_y
 
@@ -94,8 +98,23 @@ def getRandomGoal(costmap, start, max_cost, side_buffer, time_stamp, res):
             break
     return goal
 
+def poseStamped(frame_id, pose2d, ts, start):
+    retval = PoseStamped()
+    retval.header.frame_id = frame_id
+    retval.header.stamp = ts
+    retval.pose.position.x = pose2d[0]
+    retval.pose.position.y = pose2d[1]
+    return retval
+
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', nargs=3, default=None)
+    parser.add_argument('-g', nargs=3, default=None)
+    parser.add_argument('-it', default=100)
+    parser.add_argument('-p', nargs="+", default=['GridBased'])
+    args = parser.parse_args(sys.argv[1:])
+
     rclpy.init()
 
     navigator = BasicNavigator()
@@ -103,28 +122,36 @@ def main():
     # Get the costmap for start/goal validation
     costmap_msg = navigator.getGlobalCostmap()
     costmap = np.asarray(costmap_msg.data)
+    ox = costmap_msg.metadata.origin.position.x # Position of pgm-file origin relative to map
+    oy = costmap_msg.metadata.origin.position.y # Position of pgm-file origin relative to map
+    origin = np.array([ox, oy])
     costmap.resize(costmap_msg.metadata.size_y, costmap_msg.metadata.size_x)
-
-    planners = ['GridBased']
+    planners = args.p
     max_cost = 210
-    side_buffer = 100
+    side_buffer = round(np.min(costmap.shape)*0.1)
     time_stamp = navigator.get_clock().now().to_msg()
     results = []
     seed(33)
-
-    random_pairs = 100
+    random_pairs = int(args.it)
     res = costmap_msg.metadata.resolution
-    i = 0
-    while len(results) != random_pairs:
+
+    if args.s is not None or args.g is not None:
+        # Use random start/goals
+        getStart = partial(poseStamped, 'map', np.array(args.s).astype(float), time_stamp, None)
+        getGoal = partial(poseStamped, 'map', np.array(args.g).astype(float), time_stamp)
+    else:
+        getStart = partial(getRandomStart, costmap, max_cost, side_buffer, time_stamp, res, origin)
+        getGoal = partial(getRandomGoal, costmap, max_cost, side_buffer, time_stamp, res, origin)
+
+    for i in range(random_pairs):
         print("Cycle: ", i, "out of: ", random_pairs)
-        start = getRandomStart(costmap, max_cost, side_buffer, time_stamp, res)
-        goal = getRandomGoal(costmap, start, max_cost, side_buffer, time_stamp, res)
+        start = getStart()
+        goal = getGoal(start)
         print("Start", start)
         print("Goal", goal)
         result = getPlannerResults(navigator, start, goal, planners)
         if len(result) == len(planners):
             results.append(result)
-            i = i + 1
         else:
             print("One of the planners was invalid")
 
